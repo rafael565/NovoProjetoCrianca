e4926254a0d2f45bb4efb8e6cb822a96cba8b1ba
rafael565 <147094251+rafael565@users.noreply.github.com>
2024-11-19 07:57:45 -0300

    program.cs revisado

--- Program.cs
+++ Program.cs
@@ -2,34 +2,27 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NovoProjetoCrianca.Data;
 using NovoProjetoCrianca.Models;
+using System.Reflection.Emit;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddDbContext<Contexto>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("conexao")));
 
-
-// Configuração do contexto de banco de dados
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
-// Habilitar controle de erros de banco de dados durante o desenvolvimento
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Configuração do Identity com suporte a User e Role
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-    options.SignIn.RequireConfirmedAccount = true)  // Requer conta confirmada
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-
-// Adiciona suporte ao MVC (Controllers e Razor Pages)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();  // Habilita Razor Pages
 
 var app = builder.Build();
 
-// Configuração do pipeline de requisição HTTP
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -38,21 +31,15 @@ else
 {
     app.UseExceptionHandler("/Home/Error");
 }
-
-app.UseStaticFiles();  // Permite servir arquivos estáticos como CSS, JS, imagens etc.
+app.UseStaticFiles();
 
 app.UseRouting();
 
-// Ativa a autenticação e autorização
-app.UseAuthentication();  // Verifica a identidade do usuário
-app.UseAuthorization();   // Verifica permissões do usuário
+app.UseAuthorization();
 
-// Define o roteamento do MVC
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Mapeia as Razor Pages para o pipeline
 app.MapRazorPages();
 
 app.Run();
